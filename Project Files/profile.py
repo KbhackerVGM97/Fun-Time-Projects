95487f6fa64e185d72265ae137cda2b59719918f
Kemraj Sobhai <Kbsobhai@gmail.com>
2014-03-22 21:00:50 -0400

    update

--- Project Files/profile.py
+++ Project Files/profile.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python
 import cgi, cgitb
 import datetime
+import os
 #from datetime import *
 cgitb.enable()
 
 page='Content-type: text/html\n\n'
 form=cgi.FieldStorage()
-user = form[username].value
+user = form["username"].value
 
+f3 = open('profilepagetemplate.html','r')
+f1 = f3.read()
+f3.close()
+f1.replace("*****", user)
+if os.path.exists('profiles/'+user+'profileinfo.txt'):
+    f4 = open('profiles/'+user+'profileinfo.txt','r')
+    f2= f4.read()
+    f4.close()
+    L = f2.splitlines()
+    f1.replace("Enter First Name", f2[0])
+    f1.replace("Enter Last Name", f2[1])
+    f1.replace("Enter Your E-mail", f2[2])
+
+page += f1
+print page
