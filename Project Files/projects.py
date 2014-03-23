8382df6e9da6cde3e97d4a59ab60fef5c03814b0
Kemraj Sobhai <Kbsobhai@gmail.com>
2014-03-22 23:47:06 -0400

    update

--- Project Files/projects.py
+++ Project Files/projects.py
@@ -9,3 +9,21 @@ page='Content-type: text/html\n\n'
 form=cgi.FieldStorage()
 user = form["username"].value
 
+if os.path.exists('data/'+user+'/projectdata.txt'):
+    #a file with the names ofall projects
+    f1= open 'data/'+user+'/projectdata.txt'
+    f2=f1.read()
+    f1.close()
+    L2=[]
+    L1=f2.splitlines()
+    for i in L1:
+        #a file for each project with its atributes
+        if os.path.exists('data/'+user+'/'+i):
+            f1=open('data/'+user+'/'+i)
+            L2+= f1.read()
+            f1.close()
+        else:
+            L2+="***the project had no file***"
+    #L2 now contains all the project atribute files
+    
+    
