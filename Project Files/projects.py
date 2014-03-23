74f47fbdc8fc3666acd51f60a745ce1cb231a6d8
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2014-03-22 21:53:22 -0400

    worked on prjects.py

--- Project Files/projects.py
+++ Project Files/projects.py
@@ -12,22 +12,41 @@ f1=open('projectspagetemplate.html','r')
 pages+=f1.read()
 f1.close()
 #pages is a complete template where only the tables will need to be replaced
+splitpages = pages.split('<div id="insert"></div>')
+prelist = splitpages[0]
+page+=prelist
+postlist = splitpages[1]
 
 if os.path.exists('data/'+user+'/projectdata.txt'):#if the path exists
     #a file with the names of all projects
-    f1= open 'data/'+user+'/projectdata.txt'
+    f1= open ('data/'+user+'/projectdata.txt','r')
     f2=f1.read()
     f1.close()
     L2=[]
     L1=f2.splitlines()
+##    for i in L1:
+##        #a file for each project with its atributes
+##        if os.path.exists('data/'+user+'/'+i):
+##            f1=open('data/'+user+'/'+i,'r')
+##            L2+= f1.read()
+##            f1.close()
+##        else:
+##            L2+="***the project had no file***"
     for i in L1:
-        #a file for each project with its atributes
-        if os.path.exists('data/'+user+'/'+i):
-            f1=open('data/'+user+'/'+i)
-            L2+= f1.read()
-            f1.close()
-        else:
-            L2+="***the project had no file***"
-    #L2 now contains all the project atribute files
+        #L1[i] is the ith project for that user
+        L2=L1[i].split(':')
+        #L2[0] is name
+        #L2[1] is due date
+        #L2[2] is members
+        page+='<tr><td>'+L2[0]+'</td>'
+        page+='<td>'+L2[1]+'</td>'
+        page+='<td>'+L2[2]+'</td></tr>'
+else:
+    f1 = open ('data/' + user + '/projectdata.txt','w')
+    f1.close()
+
+    
+page+=postlist
+print page
     
     
