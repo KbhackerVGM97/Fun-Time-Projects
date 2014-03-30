49f1ce318abaaa1a7502780d921ba4d22712dae3
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2014-03-30 14:57:13 -0400

    check

--- Project Files/makenewproject.py
+++ Project Files/makenewproject.py
@@ -23,20 +23,22 @@ projectinfo = project + '*#*' + projectdata
 f1 = open('data/'+user+'/projectdata.txt','r')
 f2=f1.read()
 f1.close()
-f2+=project+'\n'
-f1 = open('data/'+user+'/projectdata.txt','w')
-f1.write(f2)
-f1.close()
-#writes the project file
-f1 = open('data/'+user+'/'+name+'.txt','w')
-f1.write(projectinfo)
-f1.close()
+
+projects = f2.split('\n')
+if not (project in projects):
+    f2+=project+'\n'    
+    f1 = open('data/'+user+'/projectdata.txt','w')
+    f1.write(f2)
+    f1.close()
+    #writes the project file
+    f1 = open('data/'+user+'/'+name+'.txt','w')
+    f1.write(projectinfo)
+    f1.close()
 
 f1 = open('madeprojecttemplate.html','r')
 f2=f1.read()
 f1.close()
 f2 = f2.replace('*****',user)
-
 page += f2
 print page
 
