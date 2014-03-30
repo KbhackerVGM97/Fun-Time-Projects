4678bc00c5f19406e46b204aae86a0e2b8a6dd32
Kemraj Sobhai <Kbsobhai@gmail.com>
2014-03-30 15:34:45 -0400

    update and fix

--- Project Files/makenewproject.py
+++ Project Files/makenewproject.py
@@ -11,34 +11,44 @@ form=cgi.FieldStorage()
 user = form["username"].value
 
 name = form['name'].value
-members = form['members'].value
+#memberslist=[]
+#members=user
+#try:
+members = form['members'].value+','+user
 memberslist = members.split(',')
+#except:
+ #S   pass
+
 duedate = form['duedate'].value
 projectdata = form['project'].value
 
 project = name + '*#*' + duedate + '*#*' + members
-
 projectinfo = project + '*#*' + projectdata
-#update projectdata.txt
-f1 = open('data/'+user+'/projectdata.txt','r')
-f2=f1.read()
-f1.close()
 
-projects = f2.split('\n')
-if not (project in projects):
-    f2+=project+'\n'    
-    f1 = open('data/'+user+'/projectdata.txt','w')
-    f1.write(f2)
-    f1.close()
-    #writes the project file
-    f1 = open('data/'+user+'/'+name+'.txt','w')
-    f1.write(projectinfo)
-    f1.close()
-
+for i in memberslist:
+    f2=""
+    if os.path.exists('data/'+i+'/projectdata.txt'):
+        #update projectdata.txt
+        f1 = open('data/'+i+'/projectdata.txt','r')
+        f2=f1.read()
+        f1.close()
+        projects = f2.split('\n')
+    else:
+        projects=""
+    if not (project in projects):
+        f2+=project+'\n'    
+        f1 = open('data/'+user+'/projectdata.txt','w')
+        f1.write(f2)
+        f1.close()
+        #writes the project file
+        f1 = open('data/'+user+'/'+name+'.txt','w')
+        f1.write(projectinfo)
+        f1.close()
 f1 = open('madeprojecttemplate.html','r')
 f2=f1.read()
 f1.close()
 f2 = f2.replace('*****',user)
+
 page += f2
 print page
 
